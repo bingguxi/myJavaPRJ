bbc409503bfec9f032532c35c0875ac8108504af
bingguxi <kongsuni3685@gmail.com>
2023-03-15 15:26:56 +0900

    두번째 파일

--- src/main/java/Diamond.java
+++ src/main/java/Diamond.java
@@ -2,22 +2,21 @@ public class Diamond {
     public static void main(String[] args) {
         for (int i = 1; i < 8; i++) {
             if (i < 5) {
-                for (int j = 1; j < 5 - i; j--) {
+                for (int j = 1; j < 5 - i; j++) {
                     System.out.print(" ");
                 }
-                for (int k = 1; k < 2 * i; k++) {
+                for (int k = 1; k <= (2 * i) - 1; k++) {
                     System.out.print("*");
                 }
-                System.out.println();
             } else {
-                for (int j = 1; j <= i - 5; j++) {
+                for (int j = 1; j <= i - 4; j++) {
                     System.out.print(" ");
                 }
-                for (int k = 1; k > 2 * i; k--) {
+                for (int k = 1; k <= 15 - (2 * i) ; k++) {
                     System.out.print("*");
                 }
-                System.out.println();
             }
+            System.out.println();
         }
     }
 }
