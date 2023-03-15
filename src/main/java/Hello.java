c1fa66a1bb145e82656480fe40213281fc7816d1
bingguxi <kongsuni3685@gmail.com>
2023-03-15 09:40:59 +0900

    initial commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,6 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Hello!! My First Java Coding!!");
 
-        System.out.println("Edit Java Code!!!");
+        System.out.println("This is my first no its my second java coding class");
     }
 }
