14514e0e623b5c9d3f419541ac0d2476bc32db83
bingguxi <kongsuni3685@gmail.com>
2023-03-31 20:32:43 +0900

    두번째 파일

---
+++ src/main/java/Java_quizthree.java
@@ -0,0 +1,19 @@
+public class Java_quizthree {
+    public static void main(String[] args) {
+        int i, j;
+
+        for (i = 1, j = 2; i < 6; i++)
+        {
+            if (j < 7)
+            {
+                System.out.print("*");
+            }
+            if (i == j-1)
+            {
+                System.out.println();
+                j++;
+                i = 0;
+            }
+        }
+    }
+}
