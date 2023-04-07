5557b02af808005d4400187cf0c6555bdefccf50
bingguxi <kongsuni3685@gmail.com>
2023-04-07 20:00:44 +0900

    2023.04.07 백준 4단계 1번

---
+++ src/main/java/April_seventh.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class April_seventh {
+    public static void main(String[] args) {
+        int N;
+        int i;
+        int v;
+        int count = 0;
+        Scanner s = new Scanner(System.in);
+
+        N = s.nextInt();
+        int[] a = new int[N];
+
+        for (i = 0; i < N; i++)
+        {
+            a[i] = s.nextInt();
+        }
+
+        v = s.nextInt();
+        for (i = 0; i < a.length; i++)
+        {
+
+            if (v == a[i])
+            {
+                count++;
+            }
+        }
+        System.out.printf("%d", count);
+
+    }
+}
