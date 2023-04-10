ffdf7774b8f0b138ff002d26afdd52161195f7c9
bingguxi <kongsuni3685@gmail.com>
2023-04-10 17:43:23 +0900

    2023.04.10 백준 1차원배열 2단계

---
+++ src/main/java/April_tenth.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class April_tenth {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int i, target;
+
+            i = s.nextInt();
+            int a[] = new int[i];
+            target = s.nextInt();
+
+        for (i = 0; i < a.length; i++)
+        {
+            a[i] = s.nextInt();
+
+            if (a[i] < target)
+            {
+                System.out.printf("%d ", a[i]);
+            }
+        }
+    }
+}
