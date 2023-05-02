307dbe3fda701addd71a97c445f7493d559a98a3
bingguxi <kongsuni3685@gmail.com>
2023-05-02 14:28:46 +0900

    2023.05.02 백준 입출력과 사칙연산 12단계

---
+++ src/main/java/grass_github/May_second.java
@@ -0,0 +1,10 @@
+package grass_github;
+
+public class May_second {
+    public static void main(String[] args) {
+        System.out.println("\\     /\\");
+        System.out.println(" )   (  ')");
+        System.out.println("(   /   )");
+        System.out.println(" \\ (__) |");
+    }
+}
