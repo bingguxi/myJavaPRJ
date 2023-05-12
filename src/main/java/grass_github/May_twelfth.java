5c0b5cdd7a787daff5d00c1524d0870505242c73
bingguxi <127274890+bingguxi@users.noreply.github.com>
2023-05-12 16:23:28 +0900

    2023.05.12 백준 문자열 2단계

---
+++ src/main/java/grass_github/May_twelfth.java
@@ -0,0 +1,12 @@
+package grass_github;
+
+import java.util.Scanner;
+
+public class May_twelfth {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+
+        System.out.println(str.length());
+    }
+}
