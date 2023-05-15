9db3d0e5804f8cdc1e94c5c9a25f2fd84bf67ed2
bingguxi <kongsuni3685@gmail.com>
2023-05-15 21:44:49 +0900

    2023.05.15 백준 문자열 4단계

---
+++ src/main/java/grass_github/May_fifteenth.java
@@ -0,0 +1,13 @@
+package grass_github;
+
+import java.util.Scanner;
+
+public class May_fifteenth {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String a = s.next();
+        int ascii = a.charAt(0);
+
+        System.out.print(ascii);
+    }
+}
